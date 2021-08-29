5c19e4d474993b4f7fb9a2cd09a7e3452b015c32
Paarth-Jain <paarthj3@gmail.com>
2021-08-29 10:59:49 +0530

    added temporary graph structure, and minor changes

--- classes.cpp
+++ classes.cpp
@@ -4,12 +4,134 @@ using namespace std;
 
 #define MAX_BLOCK_SIZE 1024
 
+class Txn{
+
+public:
+
+    string s;
+    int sender_id;
+    int receiver_id;
+    double amount;
+    bool coinbase;
+
+    Txn (int sid,int rid,double trans_amount, bool coinbase = false){
+        sender_id = sid;
+        receiver_id = rid;
+        amount = trans_amount;
+        this->coinbase = coinbase;
+
+        //generate appropriate string and store in s (separate cases for with and without coinbase)
+    }
+
+    Txn (string s){
+        // parse the string etc
+    }
+
+    string to_string(){
+        return s;
+    }
+
+
+};
+
+class Block{
+
+public:
+
+    uint32_t id;
+
+    vector<string> transactions; 
+
+    vector<int> balances;
+
+    uint32_t parent_id;
+
+
+    // constructor
+    Block(uint32_t parent_id, int miner_id){
+
+        // Assigning address as id
+        int new_id = rand()%INT_MAX;
+        id = new_id;
+        this->parent_id = parent_id;
+
+        // Adding coinbase transaction
+        Txn coinbase = Txn(-1, miner_id, 50, true);
+
+    }
+
+};
+
+
+class Block_node{
+
+public:
+
+    uint32_t id;
+    vector<string> transactions; 
+    vector<int> balances;
+    vector<Block_node*> children;
+    int length;         // distance from the genesis block_node
+
+    //TODO
+    vector<int> counts;  //  to track the family tree counts , should use dfs/bfs
+
+    // constructor
+    Block_node(int id, int length, vector<string> transactions, vector<int> balances){
+        this->id = id;
+        this->length = length;
+        this->transactions = transactions;
+        this->balances = balances;
+    }
+
+    // adding of child (returns bool to indicate success)
+    Block_node* add_child(Block* new_block){
+
+        // Verifying transactions with current balances
+        vector<int> temp_balances(balances);
+
+        for(string txn_string: new_block->transactions){
+            Txn txn = Txn(txn_string);
+            temp_balances[txn.sender_id] -= txn.amount;
+            if(temp_balances[txn.sender_id] < 0){
+                return NULL;
+            }
+        }
+
+        // Verification successful, making a new Block_node to add in the tree
+        Block_node* new_block_node = new Block_node(new_block->id, this->length+1, new_block->transactions, temp_balances);
+        children.push_back(new_block_node);
+
+        return new_block_node;
+    }
+
+    Block_node* find(int block_id){
+        if(id == block_id){
+            return this;
+        }
+        else{
+            Block_node* result = NULL;
+            for(Block_node* block: children){
+                result = block->find(id);
+
+                if(result != NULL) return result;
+            }
+
+            return result;
+        }
+    }
+
+};
+
+
 
 class Node{
 
+public :
+
     int id;
     int coins ; //   no of bitcoins owned so far 
-    bool heavy;
+    bool slow;
 
     vector<Node*> peers;
     vector<double> latency;
@@ -21,13 +143,11 @@ class Node{
     priority_queue<pair<string,int>>* event_queue; // To be assigned value in the contructor
     unordered_set<uint32_t> received_blocks;
 
-
-public :
-
-    Node (int new_id, int bcoin = 0){
+    Node (int new_id, bool is_slow, int bcoin = 0){
         // Assigning address as id
         id = new_id;
         coins = bcoin;
+        slow = is_slow;
     }
 
     // Method to generate new block from existing pool of transactions
@@ -72,143 +192,25 @@ public :
         }
 
         // Verification in add_child
-        bool status  = parent_node->add_child(&new_block);
-        if(status){
+        Block_node* new_block_node  = parent_node->add_child(&new_block);
+        if(new_block_node != NULL){
+            // If new longest chain is found (or existing chain is being extended)
+            // we replace the latest block
+            if(new_block_node->length > latest_block->length){
+                latest_block = new_block_node;
+            }
+
             // transmits the block to peers (pushes respective recieve events in the event_queue)
+
+
+            return true;
         }
-        return status;
+        return false;
     }
 
 
 
 };
-
-class Block{
-
-public:
-
-    uint32_t id;
-
-    vector<string> transactions; 
-
-    vector<int> balances;
-
-    uint32_t parent_id;
-
-
-    // constructor
-    Block(uint32_t parent_id, int miner_id){
-
-        // Assigning address as id
-        uint32_t new_id = reinterpret_cast<uint32_t>(this);
-        id = new_id;
-        this->parent_id = parent_id;
-
-        // Adding coinbase transaction
-        Txn coinbase = Txn(-1, miner_id, 50, true);
-
-    }
-
-
-
-
-};
-
-
-class Block_node{
-
-public:
-
-    uint32_t id;
-    vector<string> transactions; 
-    vector<int> balances;
-    vector<Block_node*> children;
-    int length;         // distance from the genesis block_node
-
-    //TODO
-    vector<int> counts;  //  to track the family tree counts , should use dfs/bfs
-
-    // constructor
-    Block_node(int id, int length, vector<string> transactions, vector<int> balances){
-        this->id = id;
-        this->length = length;
-        this->transactions = transactions;
-        this->balances = balances;
-    }
-
-    // adding of child (returns bool to indicate success)
-    bool add_child(Block* new_block){
-
-        // Verifying transactions with current balances
-        vector<int> temp_balances(balances);
-
-        for(string txn_string: new_block->transactions){
-            Txn txn = Txn(txn_string);
-            temp_balances[txn.sender_id] -= txn.amount;
-            if(temp_balances[txn.sender_id] < 0){
-                return false;
-            }
-        }
-
-        // Verification successful, making a new Block_node to add in the tree
-        Block_node* new_block_node = new Block_node(new_block->id, this->length+1, new_block->transactions, temp_balances);
-        children.push_back(new_block_node);
-
-        return true;
-    }
-
-    Block_node* find(int block_id){
-        if(id == block_id){
-            return this;
-        }
-        else{
-            Block_node* result = NULL;
-            for(Block_node* block: children){
-                result = block->find(id);
-
-                if(result != NULL) return result;
-            }
-
-            return result;
-        }
-    }
-
-};
-
-
-class Txn{
-
-public:
-
-    string s;
-    int sender_id;
-    int receiver_id;
-    double amount;
-    bool coinbase;
-
-    vector<Block> children;
-
-    Txn (int sid,int rid,double trans_amount, bool coinbase = false){
-        sender_id = sid;
-        receiver_id = rid;
-        amount = trans_amount;
-        this->coinbase = coinbase;
-
-        //generate appropriate string and store in s (separate cases for with and without coinbase)
-    }
-
-    Txn (string s){
-        // parse the string etc
-    }
-
-    string to_string(){
-        return s;
-    }
-
-
-};
-
-
 
 
 
@@ -228,7 +230,7 @@ class event{
         type = req_type;
         time = t;
     } 
-    
+
     // comparator in the main file itself
 
 };
--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "classes.cpp"
 #include <random>
+#include<bits/stdc++.h>
 
 bool operator<(const event& a ,const event& b){
 
@@ -8,8 +9,72 @@ bool operator<(const event& a ,const event& b){
     
 }
 
+struct hash_pair {
+    template <class T1, class T2>
+    size_t operator()(const pair<T1, T2>& p) const
+    {
+        auto hash1 = hash<T1>{}(p.first);
+        auto hash2 = hash<T2>{}(p.second);
+        return hash1 ^ hash2;
+    }
+};
+
 double poisson(double mean,double uni){
     return -mean*log(1-uni);
+}
+
+void make_graph(vector<Node> &network){
+    int n = network.size();
+    int node = 0;
+
+    queue<int> nodes;
+    nodes.push(node);
+
+    while(!nodes.empty()){
+        node = nodes.front();
+        nodes.pop();
+        int l = node*2+1;
+        int r = node*2+2;
+
+        if(l < n){
+            network[l].peers.push_back(&network[node]);
+            network[node].peers.push_back(&network[l]);
+            nodes.push(l);
+        }
+        if(r < n){
+            network[r].peers.push_back(&network[node]);
+            network[node].peers.push_back(&network[r]);
+            nodes.push(r);
+        }
+    }
+
+    int extra = n;
+
+    unordered_set<pair<int,int>, hash_pair> edges;
+
+    while(extra--){
+        int n1 = rand() % n;
+        int n2 = rand() % n;
+
+        if(n1 == n2) continue;
+        else if(n1 > n2){
+            if(n1 == 2*n2+1) continue;
+            else if(n1 == 2*n2+2) continue;
+        }
+        else{
+            if(n2 == 2*n1+1) continue;
+            else if(n2 == 2*n1+2) continue;
+        }
+
+        if(edges.find({n1,n2}) == edges.end()){
+            edges.insert({n1,n2});
+            edges.insert({n2,n1});
+
+            network[n1].peers.push_back(&(network[n2]));
+            network[n2].peers.push_back(&(network[n1]));
+        }
+    }
+
 }
 
 
@@ -18,14 +83,30 @@ int main(){
     //cout << "okay" << endl;
 
 
-    int n = 50 ; float z = 50 ;
+    int n = 10; 
+    float z = 50;
 
-    vector<Node> peers ;
-    for(int i =0;i<n;i++){
+    int num_slow = 10*z/100;
+    vector<bool> is_slow(n,false);
+    for(int i = 0; i < num_slow; i++){
+        is_slow[i] = true;
+    }
 
-        peers.push_back(Node(i));
-        // should take care of being slow or fast
+    random_shuffle(is_slow.begin(), is_slow.end());
 
+    vector<Node> network;
+    for(int i=0;i<n;i++){
+        network.push_back(Node(i, is_slow[i]));
+    }
+
+    make_graph(network);
+
+    for(int i = 0; i < n; i++){
+        cout << "Peers of " << i << " : ";
+        for(Node* node: network[i].peers){
+            cout << node->id << " ";
+        }
+        cout << endl;
     }
 
 
